3da517ba95b8ac0120e7ba312108465082935b09
mka <mads.kaaber@vitecsoftware.com>
2019-04-25 12:29:05 +0200

    Added Lazy loading and output of data

--- H3-CopenhagenAirport-DBFirst/H3-CopenhagenAirport-DBFirst/Program.cs
+++ H3-CopenhagenAirport-DBFirst/H3-CopenhagenAirport-DBFirst/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace H3_CopenhagenAirport_DBFirst
 {
@@ -6,7 +7,30 @@ namespace H3_CopenhagenAirport_DBFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start");
+            using (var context = new AirportDBContext())
+            {
+                var flight = context.Flight.Where(f => f.Id == 1).ToList();
+                foreach (var item in flight)
+                {
+                    Console.WriteLine("Travel time: " + item.TravelTime);
+                    foreach (var fo in item.FlightOperator)
+                    {
+                        Console.WriteLine("Departure Time: " + fo.Liftoff);
+                        Console.WriteLine("Arrival Time: "  + fo.Touchdown);
+                        Console.WriteLine("Plane Operator: " + fo.Operator.Name);
+                    }
+                    var route = context.Proute.Where(p => p.Id == item.ProuteId).ToList();
+                    foreach (var proute in route)
+                    {
+                        Console.WriteLine("Origin: " + proute.Origin);
+                        Console.WriteLine("Destination: " + proute.Destination);
+                    }
+
+                }
+            }
+            Console.WriteLine("End");
+            Console.ReadKey();
         }
     }
 }
