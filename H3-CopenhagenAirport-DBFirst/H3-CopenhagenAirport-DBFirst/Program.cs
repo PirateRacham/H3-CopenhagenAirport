270d551bf18dae0b470eb31061d55f62c7ca9dde
mka <mads.kaaber@vitecsoftware.com>
2019-04-25 11:33:59 +0200

    Initial solution creation

---
+++ H3-CopenhagenAirport-DBFirst/H3-CopenhagenAirport-DBFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H3_CopenhagenAirport_DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
